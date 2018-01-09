db8eb2a4816717957c0b7b4a73fb4e148459fec2
hatoo <hato2000@gmail.com>
2018-01-09 18:38:33 +0900

    Add comments to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,11 @@ use rustc_plugin::Registry;
 use syntax::feature_gate::AttributeType;
 
 #[plugin_registrar]
+/// Register "snippet" attribute to the compiler ignore it.
+///
+/// This function is called automatically.
+///
+/// See [here](https://doc.rust-lang.org/nightly/unstable-book/language-features/plugin.html).
 pub fn plugin_registrar(reg: &mut Registry) {
     reg.register_attribute("snippet".to_owned(), AttributeType::Whitelisted);
 }
