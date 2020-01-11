35f130a7cb1afa10f38563492da02e910c3422a8
hatoo <hato2000@gmail.com>
2020-01-11 23:11:37 +0900

    Use proc_macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,8 @@
-#![crate_type = "dylib"]
-#![feature(plugin_registrar, rustc_private)]
+extern crate proc_macro;
 
-extern crate rustc_driver;
-extern crate rustc_plugin;
-extern crate syntax;
-extern crate syntax_pos;
+use crate::proc_macro::TokenStream;
 
-use rustc_plugin::Registry;
-use syntax::feature_gate::AttributeType;
-use syntax_pos::symbol::Symbol;
-
-#[plugin_registrar]
-/// Register "snippet" attribute to the compiler ignore it.
-///
-/// This function is called automatically.
-///
-/// See [here](https://doc.rust-lang.org/nightly/unstable-book/language-features/plugin.html).
-pub fn plugin_registrar(reg: &mut Registry) {
-    reg.register_attribute(Symbol::intern("snippet"), AttributeType::Whitelisted);
+#[proc_macro_attribute]
+pub fn snippet(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
 }
