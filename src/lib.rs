c7947403fab217f6b230a6c9c348b0b7a91ebd70
hatoo <hato2000@gmail.com>
2020-01-12 17:24:25 +0900

    Comment

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,7 @@ use crate::proc_macro::TokenStream;
 
 #[proc_macro_attribute]
 pub fn snippet(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    // Just register `snippet` attribute.
+    // We do nothing here.
     item
 }
